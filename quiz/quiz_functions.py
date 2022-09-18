312815d19db90a127e50ca043da4453d90b4379c
Keita OCHIAI <s1f102000196@iniad.org>
2022-09-19 01:56:56 +0900

    Add comments

--- quiz/quiz_functions.py
+++ quiz/quiz_functions.py
@@ -5,9 +5,9 @@ from quiz.models import Questions, UserAnswers, Quizzes, UserData, QuizEvents, U
 # 戻り値: 辞書型で正解者と不正解者に分かれたユーザのUUIDのリスト。
 def get_scored_users(target_quiz_uuid):
     target_quiz = Quizzes.objects.get(id=target_quiz_uuid)
-    target_answers = UserAnswers.objects.filter(quiz=target_quiz)
-    correctly_answered_users = target_answers.filter(choice=target_quiz.question.correctChoice).values_list("user", flat=True)
-    incorrectly_answered_users = target_answers.exclude(choice=target_quiz.question.correctChoice).values_list("user", flat=True)
+    target_answers = UserAnswers.objects.filter(quiz=target_quiz)  # 引数のクイズに対する解答のみに絞り込み。
+    correctly_answered_users = target_answers.filter(choice=target_quiz.question.correctChoice).values_list("user", flat=True)  # 正しい解答と等しいもののみに
+    incorrectly_answered_users = target_answers.exclude(choice=target_quiz.question.correctChoice).values_list("user", flat=True)  # 正しい回答と異なるもののみに
     scored_users = {
         "correct": correctly_answered_users,
         "incorrect": incorrectly_answered_users,
@@ -23,18 +23,18 @@ def get_scored_users(target_quiz_uuid):
 # 戻り値: None
 def users_add_score(target_event_id, target_users_uuid, points, inc_correctNums_flg=True):
     target_event = QuizEvents.objects.get(pk=target_event_id)
-    event_joined_users = UserScores.objects.filter(event=target_event_id)
+    event_joined_users = UserScores.objects.filter(event=target_event_id)  # 引数の開催回のみのスコアに絞り込み。
     
-    for target_uuid in target_users_uuid:
+    for target_uuid in target_users_uuid:  # 指定した各ユーザに対して
         try:
-            target_user = event_joined_users.get(user=target_uuid)
-        except UserScores.DoesNotExist:
+            target_user = event_joined_users.get(user=target_uuid)  # 指定した開催回に対する、スコアが存在すれば取得。
+        except UserScores.DoesNotExist:  # 指定した開催回に対する、スコアが存在しなければ作成。
             user_obj = UserData.objects.get(id=target_uuid)
             target_user = UserScores(event=target_event, user=user_obj)
         
-        target_user.score += points
+        target_user.score += points  # 加点(あるいは減点)
         if inc_correctNums_flg:
-            target_user.correctNums += 1
+            target_user.correctNums += 1  # フラグが立っていればカウント
         target_user.save()
 
 # 対象ユーザに対してスコアの初期化を行う。
@@ -45,19 +45,21 @@ def users_add_score(target_event_id, target_users_uuid, points, inc_correctNums_
 # 戻り値: None
 def users_reset_score(target_event_id, target_users_uuid, score_flg=False, correctNums_flg=False):
     target_event = QuizEvents.objects.get(pk=target_event_id)
-    event_joined_users = UserScores.objects.filter(event=target_event_id)
+    event_joined_users = UserScores.objects.filter(event=target_event_id)  # 引数の開催回のみのスコアに絞り込み。
     
     for target_uuid in target_users_uuid:
         try:
-            target_user = event_joined_users.get(user=target_uuid)
-        except UserScores.DoesNotExist:
+            target_user = event_joined_users.get(user=target_uuid)  # 指定した開催回に対する、スコアが存在すれば取得。
+        except UserScores.DoesNotExist:  # 指定した開催回に対する、スコアが存在しなければ作成。
             user_obj = UserData.objects.get(id=target_uuid)
             target_user = UserScores(event=target_event, user=user_obj)
         
+        # フラグが立っていれば初期化
         if score_flg:
             target_user.score = 0
         if correctNums_flg:
             target_user.correctNums = 0
+        
         target_user.save()
 
 # 特定の開催回、ユーザのスコアを取得する。
@@ -72,8 +74,9 @@ def get_user_score(target_event_id, target_user_uuid):
 # target_event_id: クイズ大会の開催回のID。
 # この関数を実行時に、UserScoresのtemp_rankが更新される。
 def update_ranking(target_event_id):
-    event_joined_users = UserScores.objects.filter(event=target_event_id).order_by("-score")
+    event_joined_users = UserScores.objects.filter(event=target_event_id).order_by("-score")  # 指定した開催回のユーザのスコアを、scoreが高い順で取得。
 
+    # 同率順位を計算しながらランキングを更新。
     rank = 0
     before_score = None
     tie_count = 1
