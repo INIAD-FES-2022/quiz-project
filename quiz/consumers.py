4e986652011b508d05145bb93faa10662eb0dab3
Takuho3650 <s1f102101536@iniad.org>
2022-09-21 10:02:13 +0900

    update consumer
    グループに必要なコードの追加
    回答を受け取った際、保存処理の実行追加
    正誤判定やランキングを返すように
    
    不足している機能やバグ等あれば教えていただけると幸いです。

--- quiz/consumers.py
+++ quiz/consumers.py
@@ -2,9 +2,13 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 import uuid
 
+from quiz.models import UserAnswers
+from quiz_functions import *
+
 # QuizConsumerクラス: WebSocketからの受け取ったものを処理するクラス
 class QuizConsumer( AsyncWebsocketConsumer ):
-    groups = ['INIAD_FES_06_quiz_group']
+    groups = []
+    static_group = ['INIAD_FES_06_quiz_group']
 
     # WebSocket接続時の処理
     async def connect( self ):
@@ -15,6 +19,12 @@ class QuizConsumer( AsyncWebsocketConsumer ):
         except:
             self.uuid_str = str(uuid.uuid4())
         print(self.uuid_str)
+        print(self.room_group_name)
+        print(self.channel_name)
+        print(QuizConsumer.groups)
+        print(QuizConsumer.static_group)
+
+        QuizConsumer.groups.append(self.uuid_str)
 
         # 全体送信用グループ
         await self.channel_layer.group_add(
@@ -35,6 +45,11 @@ class QuizConsumer( AsyncWebsocketConsumer ):
             self.room_group_name,
             self.channel_name
         )
+        await self.channel_layer.group_discard(
+            self.uuid_str,
+            self.channel_name
+        )
+        QuizConsumer.groups.remove(self.uuid_str)
 
     # WebSocketがデータを受信した時の処理
     async def receive( self, text_data ):
@@ -44,15 +59,60 @@ class QuizConsumer( AsyncWebsocketConsumer ):
 
         # 管理者がデータを送信した場合の処理
         if(user.is_authenticated):
+            # 中間、最終発表の際にランキングを更新する。
+            if(text_data_json.get("messageType") == "userIdSentRequest"):
+                eId = text_data_json["eventId"]
+                update_ranking(eId)
+
             # 受信処理関数の追加
             text_data_json["type"]="spread_send"
             await self.channel_layer.group_send( self.room_group_name, text_data_json )
 
         # 参加者がデータを送信した場合の処理
         else:
-            # 受信処理関数の追加
-            text_data_json["type"]="spread_send"
-            await self.channel_layer.group_send( self.room_group_name, text_data_json )
+            if(text_data_json.get("messageType") == "answerSent"):
+                # 回答の保存
+                uId = text_data_json["userId"]
+                qId = text_data_json["quizId"]
+                cId = text_data_json["choice"]
+                obj = UserAnswers(user=uId, quiz=qId, choice=cId)
+                obj.save()
+                
+                # 正誤判定を行ってそれぞれに返す
+                if cId == Quizzes.objects.get(id=qId).correctChoice:
+                    data = { 
+                        "messageType": "scoringResult", 
+                        "correctChoice": cId, 
+                        "isCorrect": True
+                    }
+                    await self.send( text_data = json.dumps( data ) )
+                else:
+                    data = { 
+                        "messageType": "scoringResult", 
+                        "correctChoice": Quizzes.objects.get(id=qId).correctChoice, 
+                        "isCorrect": False
+                    }
+                    await self.send( text_data = json.dumps( data ) )
+                
+                # 加点の一斉処理を行うタイミングが不明なため、実装のほどよろしくお願いします。
+
+            elif(text_data_json.get("messageType") == "userIdSent"):
+                eId = text_data_json["eventId"]
+                uId = text_data_json["userId"]
+                isFin = text_data_json["isFin"]
+                uScore = get_users_score(eId, uId)
+                data = {
+                    "messageType": "rankDisplay",
+                    "rank": uScore.temp_rank,
+                    "score": uScore.score,
+                    "isFin": isFin,
+                }
+                self.send( text_data = json.dumps( data ) )
+
+            else:
+                # 受信処理関数の追加
+                text_data_json["type"]="spread_send"
+                await self.channel_layer.group_send( self.room_group_name, text_data_json )
 
     # 拡散データ受信時の処理
     # （self.channel_layer.group_send()の結果、グループ内の全コンシューマーにメッセージ拡散され、各コンシューマーは本関数で受信処理します）
