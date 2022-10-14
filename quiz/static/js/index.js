2c005b0d851dbccad3356a9c07553325c46e68d3
Naoto Matsumoto <s1F102001243@iniad.org>
2022-10-14 16:25:44 +0900

    トップページ完成

--- quiz/static/js/index.js
+++ quiz/static/js/index.js
@@ -3,20 +3,36 @@ let params;
 let uuid = '';
 let nickname = '';
 
+/* UUID生成 */
+function createUuid(){
+
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(a) {
+      let r = (new Date().getTime() + Math.random() * 16)%16 | 0, v = a == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+   });
+
+}
+
+
 /* URLのクエリパラメータからUUIDとニックネームの取得を試みる */
 try {
     params = location.search;
     uuid = params.get('uuid');
     nickname = params.get('nickname');
 } catch(e) {
-    console.error("Error:",e.message);
+    uuid = createUuid();
 }
     
 /* フォームにニックネームを挿入 */
-let nameForm = document.getElementById("nickname");
-nameForm.value = nickname;
+document.getElementById("nickname").value = nickname;
 
 /* sessionStorageにUUIDを保持 */
-window.sessionStorage("UUID", uuid);
+window.sessionStorage.setItem("uuid", uuid);
 
-
+/* 参加ボタンを押した時の動作 */
+let nameFormButton = document.getElementById("join-button");
+nameFormButton.addEventListener('click', function () {
+    let nameForm = document.getElementById("nickname");
+    window.sessionStorage.setItem("nickname", nameForm.value);
+    window.location.href = 'quiz_play';
+})
