e29504150b8140a60ddbb49f3ef5053068c6cf43
Naoto Matsumoto <s1F102001243@iniad.org>
2022-10-22 13:38:55 +0900

    UPDATE quiz/urls.py

--- daphne/quiz/urls.py
+++ daphne/quiz/urls.py
@@ -2,13 +2,13 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.ControlQuizTop.as_view(), name="control_quiz_top"),
-    path("control/", views.ControlQuizEvents.as_view(), name="control_quiz_events"),
-    path("control/<int:pk>", views.ControlQuizEventsDetail.as_view(), name="control_quiz_events_detail"),
+    path("control/", views.ControlQuizTop.as_view(), name="control_quiz_top"),
+    path("control/events", views.ControlQuizEvents.as_view(), name="control_quiz_events"),
+    path("control/events/<int:pk>", views.ControlQuizEventsDetail.as_view(), name="control_quiz_events_detail"),
     path("control/<int:pk>/add_quiz", views.ControlQuizEventsAddQuiz.as_view(), name="control_quiz_events_add_quiz"),
     path("control/history", views.ControlQuizHistory.as_view(), name="control_quiz_history"),
     path("control/operate", views.ControlQuizOperate.as_view(), name="control_quiz_operate"),
-    path("index", views.IndexView.as_view(), name="index"),
+    path("", views.IndexView.as_view(), name="index"),
     path("quiz_play", views.QuizPlayView.as_view(), name="join"),
     path("debug/", views.debugTop),
     path("debug/questions/", views.dbgQuestionsList.as_view(), name="questions_list"),
