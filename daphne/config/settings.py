248df1b08c2232d5b8b5ec339678b82acc07fca7
Naoto MATSUMOTO <s1F102001243@iniad.org>
2023-10-19 13:20:13 +0900

    redisのポート番号が文字列として渡されていた問題を修正

--- daphne/config/settings.py
+++ daphne/config/settings.py
@@ -171,7 +171,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [(os.environ.get('REDIS_HOST'), os.environ.get('REDIS_PORT'))], 
+            'hosts': [(os.environ.get('REDIS_HOST'), int(os.environ.get('REDIS_PORT')))], 
         }
     },
 }
